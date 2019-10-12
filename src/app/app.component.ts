6f3860edd6b1d4bd318bb36c5e5a66ca5264d7b1
David Seek <davidseek1986@gmail.com>
2019-10-11 18:04:27 -0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import {
+  Component
+} from '@angular/core';
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {
+  Platform
+} from '@ionic/angular';
+import {
+  SplashScreen
+} from '@ionic-native/splash-screen/ngx';
+import {
+  StatusBar
+} from '@ionic-native/status-bar/ngx';
+
+import {
+  Intercom
+} from "capacitor-intercom";
+const intercom = new Intercom();
 
 @Component({
   selector: 'app-root',
@@ -22,6 +35,30 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      const registrationOptions = {
+        userId: 'GBTrGjkpBIVQme8ZysKXIxC3ci43'
+      }
+
+      intercom.registerIdentifiedUser(registrationOptions)
+        .then(() => {
+
+          console.log('Intercom init success')
+        })
+        .catch(error => {
+
+          console.log('Intercom init error: ', error)
+        })
+
+      intercom.displayLauncher()
+        .then(() => {
+
+          console.log('Intercom launcher display success')
+        })
+        .catch(error => {
+
+          console.log('Intercom launcher display error: ', error)
+        })
     });
   }
 }
